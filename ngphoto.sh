98538ae2fcb848c907af4073105e7872287044ca
Shuvankar Sarkar <sonu041@gmail.com>
2018-02-03 13:33:56 +0530

    Added https url

--- ngphoto.sh
+++ ngphoto.sh
@@ -3,19 +3,23 @@
 # Author: Shuvankar Sarkar
 # Date: 25 July 2017
 
-img="$(curl http://www.nationalgeographic.com/photography/photo-of-the-day/ -s | grep -o "aemLeadImage':[^\]*" | grep -o "http://[a-zA-Z0-9./?=_-]*")"
-currdate=$(date +"%d%m%Y")
+img="$(curl https://www.nationalgeographic.com/photography/photo-of-the-day/ -s | grep -o "aemLeadImage':[^\]*" | grep -o "https://[a-zA-Z0-9./?=_-]*")"
+currdate=$(date +"%Y%m%d")
+img_file="/Users/shuvankar/Desktop/Shuvankar/PhotoGrabber/$currdate.jpg"
+imgpath="/Users/shuvankar/Desktop/Shuvankar/PhotoGrabber/$img_file"
 echo $currdate
 if [ -n "$img" ]
 then
-	img_file="$currdate.jpg"
-  echo "Downloading $img"
+
+	echo "Downloading $img"
 	if [ -f "$img_file" ]
 	then
 		echo "File already exists"
 	else
-    curl -o $img_file $img
+		curl -o $img_file $img
 		echo "Wallpaper downloaded successfully and saved as $img_file"
+		#Change Wallpaper: MAC
+		#osascript -e 'tell application "Finder" to set desktop picture to POSIX file $imgpath'
 	fi
 else
 	echo "No Wallpaper today"
