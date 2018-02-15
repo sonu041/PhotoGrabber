8b9106f6e89733d73495925164a1dab56a9334d3
Shuvankar Sarkar <sonu041@gmail.com>
2018-02-16 01:25:48 +0530

    Restructure the code flow. Avoid unnecessary network call if photo of the day is already downloaded

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PhotoGrabber
 Shell script to automatically download photo of the day from nationalgeographic.com
+
+#YouTube video explained in Bengali
+https://youtu.be/NpaVRELeKaM
+(Sorry for non Bengali - May be I will upload a english transcript soon)
--- ngphoto.sh
+++ ngphoto.sh
@@ -3,24 +3,21 @@
 # Author: Shuvankar Sarkar
 # Date: 25 July 2017
 
-img="$(curl https://www.nationalgeographic.com/photography/photo-of-the-day/ -s | grep -o "aemLeadImage':[^\]*" | grep -o "https://[a-zA-Z0-9./?=_-]*")"
 currdate=$(date +"%Y%m%d")
 img_file="/Users/shuvankar/Desktop/Shuvankar/PhotoGrabber/$currdate.jpg"
-imgpath="/Users/shuvankar/Desktop/Shuvankar/PhotoGrabber/$img_file"
-echo $currdate
-if [ -n "$img" ]
+#echo $img_file
+#echo $currdate
+if [ -f "$img_file" ]
 then
-
-	echo "Downloading $img"
-	if [ -f "$img_file" ]
+	echo "File already exists"
+else
+	img="$(curl https://www.nationalgeographic.com/photography/photo-of-the-day/ -s | grep -o "aemLeadImage':[^\]*" | grep -o "https://[a-zA-Z0-9./?=_-]*")"
+	if [ -n "$img" ]
 	then
-		echo "File already exists"
-	else
+		echo "Downloading $img"
 		curl -o $img_file $img
 		echo "Wallpaper downloaded successfully and saved as $img_file"
-		#Change Wallpaper: MAC
-		#osascript -e 'tell application "Finder" to set desktop picture to POSIX file $imgpath'
+	else
+		echo "No Wallpaper today"
 	fi
-else
-	echo "No Wallpaper today"
 fi
